2ca0bfcdf6e795bfba06a7f84bb4b932c5f7a5f2
SirZenith <sirzenith@163.com>
2021-09-10 20:20:17 +0800

    Add para -l

--- analyze.py
+++ analyze.py
@@ -29,7 +29,7 @@ def make_plot(target: TransicationRecord):
 
     plt.subplot(4, 1, 3)
     plt.title('Supply rate', fontsize='small')
-    plt.plot(target.supply_rate)
+    plt.plot(target.supply_rate_all)
 
     plt.subplot(4, 1, 4)
     plt.title('Local burst', fontsize='small')
@@ -57,6 +57,7 @@ def write_csv(tc: "list[TransicationRecord]", filename: str):
                 performance(target),
             ])
 
+
 def printinfo(target: TransicationRecord):
     print(f'供应商 ID：{target.id}')
     print(f'供货量均值：{target.supply.mean()}')
@@ -64,6 +65,38 @@ def printinfo(target: TransicationRecord):
     print(f'供货差均值：{target.supply_delta}')
     print(f'   履约率：{target.long_term_supply_rate}')
     print(f'履约率方差：{target.supply_rate.var()}')
+
+
+def circle_map(x):
+    if x > 1:
+        x = 1 - x
+    elif x > 0:
+        x = 1 - 1 / x
+    else:
+        raise ValueError('input argument should be greater than zero')
+    return np.exp(x)
+
+
+def rate_leap(target: TransicationRecord) -> np.ndarray:
+    req_ratio = np.fromiter(
+        (0 if v1 * v2 < 1 else v2 / v1 for v1, v2 in zip(target.requests[:-1], target.requests[1:])),
+        dtype=float
+    )
+    rate_diff = np.diff(target.supply_rate_all)
+    diff = np.abs(rate_diff) * req_ratio
+    return diff
+    
+
+def all_rate_leap(tc: "list[TransicationRecord]") -> "list[np.ndarray]":
+    results = []
+    # conv_vec = TransicationRecord.local_conv_vec(25)
+    for t in tc:
+        diff = rate_leap(t)
+        # local = np.convolve(conv_vec, diff, mode='same')
+        # if np.any(np.abs(diff - local) > 0.2):
+            # results.append((i, diff))
+        results.append(diff)
+    return results
 
 
 if __name__ == '__main__':
@@ -77,9 +110,18 @@ if __name__ == '__main__':
     tp = TransportRecord.from_pickled(transport_bin)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--plot', default=None, type=int, metavar='<id>', help='plot data with given supplier id.')
-    parser.add_argument('-o', '--output', default=None, type=str, metavar='<file name>', help='write data to csv file')
-    parser.add_argument('-i', '--info', default=None, type=int, metavar='<id>', help='print infomation with given id.')
+    parser.add_argument('-p', '--plot', default=None, type=int,
+                        metavar='<id>', help='plot data with given supplier id.')
+    parser.add_argument('-o', '--output', default=None, type=str,
+                        metavar='<file name>', help='write data to csv file')
+    parser.add_argument('-i', '--info', default=None, type=int,
+                        metavar='<id>', help='print infomation with given id.')
+    parser.add_argument('-d', '--diff', action='store_true')
+
+    parser.add_argument('-l', '--leap', default=None, type=int,
+                        metavar='<id>', help='compute leap value in requests amount')
+    parser.add_argument('-L', '--all-leap', action='store_true', dest='all_leap',
+                        help='drawing scatter plot for leap point count for all supplier')
 
     args = parser.parse_args()
     if args.plot is not None:
@@ -88,3 +130,21 @@ if __name__ == '__main__':
         write_csv(tc, args.output)
     if args.info is not None:
         printinfo(tc[args.info - 1])
+    if args.leap is not None:
+        target = tc[args.leap - 1]
+        leap = rate_leap(target)
+        plt.plot(leap)
+        plt.plot(np.array([leap.mean()] * leap.size))
+        plt.title(target.id)
+        plt.show()
+    if args.all_leap:
+        leaps = all_rate_leap(tc)
+        leap_count = [np.count_nonzero(l > 20) for l in leaps]
+        plt.plot(np.array(leap_count))
+        plt.title('Leap Count')
+
+        output  = sorted(range(1, len(leaps) + 1), key=lambda i: leap_count[i - 1])
+        for i in output:
+            print(i)
+
+        plt.show()
