68fcf1da49817727c762c0ade877b5fe565c828e
SirZenith <sirzenith@163.com>
2021-09-10 11:16:36 +0800

    添加类型文档字符串，删去部分冗余成员

--- analyze.py
+++ analyze.py
@@ -14,19 +14,19 @@ if __name__ == '__main__':
     tc = TransicationRecord.from_pickled(transication_bin)
     tp = TransportRecord.from_pickled(transport_bin)
 
-    tc.sort(key=lambda r: r.supply_rate * r.mean, reverse=True)
+    tc.sort(key=lambda r: r.long_term_supply_rate * r.supply.mean(), reverse=True)
     target = tc[1]
 
     # print(target.supply_rate_data)
     # mask = target.requests > 2.2e-16
     # print(target.supply[mask] - target.requests[mask])
     print(f'供应商 ID：{target.id}')
-    print(f'供货量均值：{target.mean}')
+    print(f'供货量均值：{target.supply.mean()}')
     print(f'  订单总量：{target.requests[target.requests >= 1].size}')
     print(f'供货差均值：{target.supply_delta}')
     print(f'     周期：{target.freqs.argmin() + 1}')
-    print(f'   履约率：{target.supply_rate}')
-    print(f'履约率方差：{target.supply_rate_var}')
+    print(f'   履约率：{target.long_term_supply_rate}')
+    print(f'履约率方差：{target.supply_rate.var}')
 
     plt.figure()
     plt.subplot(3, 1, 1)
--- modeling.py
+++ modeling.py
@@ -41,7 +41,21 @@ class Record(object):
 
 class TransicationRecord(Record):
     """TransicationRecord records requests sent, or resource supplied in past 240
-    weeks."""
+    weeks.
+    
+    Attribute:
+        id: str, id of supplier.
+        src_type: SrcType, source type of this supplier.
+        supply: numpy.ndarray, array of supply data.
+        requests: numpy.ndarray, array of requests data.
+        freqs: numpy.ndarray, arrat of frequency identify data, frequency closer
+               to frequency in under laying data will lead to smaller identify
+               data.
+        supply_delta: numpy.ndarray, difference between supply and request, only
+                      non-zero requests are counted.
+        supply_rate: numpy.ndarray, supply rate of each request.
+        long_term_supply_rate: float, ratio of sum of supply data to sum of requests.
+    """
     SUPPLIER_COUNT = 402
 
     def __init__(
@@ -56,14 +70,12 @@ class TransicationRecord(Record):
         self.src_type = src_type
         self.supply = np.array(supply_data)
         self.requests = np.array(requests_data) if requests_data else None
-        self.mean = self.supply.mean()
         self.freqs = np.array([
             abs(v @ self.supply.T) / Record.WEEK_COUNT for v in loop_vectors
         ])
         self.supply_delta = None
-        self.supply_rate_data = None
+        self.supply_rate = None
         self.long_term_supply_rate = None
-        self.supply_rate_var = None
 
     @classmethod
     def from_csv(
@@ -101,17 +113,21 @@ class TransicationRecord(Record):
         if not np.any(mask):
             return
         self.supply_delta = (self.supply[mask] - self.requests[mask]).mean()
+        self.supply_rate = np.array(self.supply[mask] / self.requests[mask] * 100)
         self.long_term_supply_rate = self.supply.sum() / self.requests.sum() * 100
-        supply_rate = self.supply[mask] / self.requests[mask] * 100
-        supply_rate = np.array(supply_rate)
-        self.supply_rate_data = supply_rate
-        self.supply_rate_var = supply_rate.var()
 
 
 class TransportRecord(Record):
+    """TransportRecord records transportation data.
+    
+    Attribute:
+        id: str, id of a transport company.
+        data: numpy.ndarray, cost of this company is past weeks.
+    """
     TRANSPORT_COUNT = 8
 
-    def __init__(self, data: "list[float]"):
+    def __init__(self, id: str, data: "list[float]"):
+        self.id = id
         self.data = np.array(data)
 
     @classmethod
@@ -125,5 +141,5 @@ class TransportRecord(Record):
             for row in reader:
                 supply_id = int(row[0][1:]) - 1
                 data = [float(i) for i in row[1:]]
-                results[supply_id] = TransportRecord(data)
+                results[supply_id] = TransportRecord(row[0], data)
         return results
